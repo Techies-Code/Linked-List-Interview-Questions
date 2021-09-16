b8a52e95016aaded4dc45fc9d6458db1d12211f0
Saurabh Joshi <joshisaurabh2610@gmail.com>
2021-09-16 16:01:45 +0530

    Copy List Random Pointer Approach 2 Space Optimised

--- CopyListRandom.cpp
+++ CopyListRandom.cpp
@@ -18,7 +18,7 @@ public:
 Node* copyRandomList(Node* head) {
 
 	Node* curr = head;
-	while(curr){
+	while (curr) {
 		Node*n = new Node(curr->val);
 		// insert this node just after cur
 		Node* temp = curr->next;
@@ -29,7 +29,7 @@ Node* copyRandomList(Node* head) {
 	}
 
 	curr = head;
-	while(curr){
+	while (curr) {
 		Node* copyNode = curr->next;
 		Node* nxt = copyNode->next;
 		copyNode->random = curr->random ->next;
@@ -39,4 +39,59 @@ Node* copyRandomList(Node* head) {
 
 	return head->next;
 
+}
+
+// Approach 2ḍ
+Node* copyRandomList(Node* head) {
+
+	if(head == nullptr)
+		return head;
+	
+	// iteration 1 -:   create new nodes and insert just after curr nodes
+	Node*curr = head;
+
+	while(curr){
+		Node* n = new Node(curr->val);
+
+		// inserst just after curr node
+		Node*nxt = curr->next;
+		curr->next = n;
+		n->next = nxt;
+		
+		// for next iteration
+		curr = nxt;
+	}
+
+	// iteration 2 adjust the random pointers
+	curr = head;
+	while(curr){
+		Node* curr_copy = curr->next;
+		Node*nxt = curr_copy->next;
+
+		if(curr->random)
+			curr_copy->random = curr->random ->next;
+
+		// for next iteration
+		curr = nxt;
+	}
+
+	// iteration 3 readjust next pointer 
+	// to separate the list
+	Node*newHead = nullptr;
+
+	curr = head;
+	while(curr){
+		Node* curr_copy = curr->next;
+		Node*nxt = curr_copy->next;
+
+		if(curr == head)
+			newHead = curr_copy;
+
+		if(nxt)
+			curr_copy->next = nxt->next;
+		curr->next = nxt;
+
+		curr = nxt;
+	}
+return newHead;
 }
